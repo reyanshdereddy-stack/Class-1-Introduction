5acd948caceb4330d52bec6270dc2f2f206ae640
reyanshdereddy-stack <reyanshdereddy@gmail.com>
2025-12-09 19:19:51 -0500

    v2

--- Program.java
+++ Program.java
@@ -1,5 +1,8 @@
 class Program{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a=6, b=2, c;
+        c=a+b;
+        System.out.println("Addition is "+c);
     }
 }
